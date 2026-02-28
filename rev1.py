71c937de3b66cf726997a2de682a54f0f0d8ac30
neonx7-me <Kareemshirsho21@gmail.com>
2026-02-28 23:21:50 +0600

    Add files via upload

---
+++ rev1.py
@@ -0,0 +1,7 @@
+for i in range(999999999):
+    print(i)
+
+count = 5 
+while count < 5:
+    print(count)
+    count += 1
---
+++ rev2.py
@@ -0,0 +1,9 @@
+def greet(name):
+    return f"Hello, {name}!"
+print(greet("Alice"))
+
+
+def add(a, b):
+    result = a * b
+    return result
+print(add(999999, 5000000000))
